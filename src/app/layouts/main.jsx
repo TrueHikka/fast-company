65bc8ae1df2ed87eac31555fe1de2a883bfb10db
Vitaly <vitalyrubis@gmail.com>
2022-10-06 17:51:17 +0300

    mockData, main

--- src/app/layouts/main.jsx
+++ src/app/layouts/main.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import useMockData from "../utils/mockData";
 
 const Main = () => {
-    const { initialize } = useMockData();
+    const {error, initialize, progress, status} = useMockData();
 
     const handleClick = () => {
-        console.log("clicked");
         initialize();
     };
 
@@ -13,6 +12,11 @@ const Main = () => {
         <div className="container mt-5">
             <h1>Main Page</h1>
             <h3>Инициализация данных в FireBase</h3>
+			<ul>
+				<li>Status: {status}</li>
+				<li>Progress: {progress}%</li>
+				{error && <li>error:{error}</li>}
+			</ul>
             <button className="btn btn-primary" onClick={handleClick}>
                 Инициализировать
             </button>
--- src/app/utils/mockData.js
+++ src/app/utils/mockData.js
@@ -1,53 +1,69 @@
-import { useEffect, useState } from "react";
-import professions from "../mockData/professions.json";
-import qualities from "../mockData/qualities.json";
-import users from "../mockData/users.json";
-import httpService from "../services/http.service";
+import { useEffect, useState } from "react"
+import professions from "../mockData/professions.json"
+import qualities from "../mockData/qualities.json"
+import users from "../mockData/users.json"
+import httpService from "../services/http.service"
 
 const useMockData = () => {
-    const statusConsts = {
-        idle: "NotStarted",
-        pending: "In Process",
-        successed: "Ready",
-        error: "Error occured"
-    };
+	const statusConsts = {
+idle:"Not Started",
+pending: "In Process",
+successed: "Ready",
+error: "Error occured",
+	}
 
-    const [error, setError] = useState(null);
-    const [status, setStatus] = useState();
-    const [progress, setProgress] = useState(0);
-    const [count, setCount] = useState(0);
+const [error, setError] = useState(null);
+	const [status, setStatus] = useState(statusConsts.idle)
+	const [progress, setProgress] = useState(0);
+	const [count, setCount] = useState(0);
+	
+	const summuryCount = professions.length + qualities.length + users.length
+	
+const incrementCount = () => {
+setCount(prev=>prev+1)
+}
 
-    const summuryCount = professions.length + qualities.length + users.length;
+const updateProgress = () => {
+	if(count !==0 && status === statusConsts.idle){
+		setStatus(statusConsts.pending)
+	}
+	
+	const newProgress = Math.floor((count/summuryCount)*100)
+	if(progress<newProgress){
+		setProgress(()=>newProgress)
+	}
 
-    const incrementCount = () => {
-        setCount((prev) => prev + 1);
-    };
+	if(newProgress===100){
+		setStatus(statusConsts.successed)
+	}
+}
 
-    const updateProgress = () => {};
+useEffect(()=>{
+	updateProgress()
+}, [count])
 
-    useEffect(() => {
-        if (count !== 0 && status === statusConsts.idle) {
-            setStatus(statusConsts.pending);
-        }
+async function initialize(){
+	try {
+		for(const prof of professions){
+			await httpService.put("profession/" + prof._id, prof)
+			incrementCount()
+		}
+		for(const user of users){
+			await httpService.put("user/" + user._id, user)
+			incrementCount()
+		}
+		for(const qual of qualities){
+			await httpService.put("quality/" + qual._id, qual)
+			incrementCount()
+		}
+	} catch (error) {
+		setError(error)
+		setStatus(statusConsts.error)
+	}
 
-        const newProgress = Math.floor((count / summuryCount) * 100);
-        if (progress < newProgress) {
-            setProgress(() => newProgress);
-        }
-    }, [count]);
+}
 
-    async function initialize() {
-        try {
-            for (const prof of professions) {
-                await httpService.put("profession/" + prof._id, prof);
-                incrementCount();
-            }
-        } catch (error) {
-            setError(error);
-        }
-    }
-
-    return { error, initialize };
-};
-
+	return {error, initialize, progress, status};
+}
+ 
 export default useMockData;
