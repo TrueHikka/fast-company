6983ca7302561f068b55bc791c0601250149e276
Vitaly <vitalyrubis@gmail.com>
2022-11-05 15:08:36 +0300

    Получение актуальных данных

--- src/app/store/qualities.js
+++ src/app/store/qualities.js
@@ -6,7 +6,8 @@ const qualitiesSlice = createSlice({
     initialState: {
         entities: null,
         isLoading: true,
-        error: null
+        error: null,
+		lastFetch: null
     },
     reducers: {
         qualitiesRequested: (state) => {
@@ -14,6 +15,7 @@ const qualitiesSlice = createSlice({
         },
         qualitiesReceved: (state, action) => {
             state.entities = action.payload;
+			state.lastFetch = Date.now()
             state.isLoading = false;
         },
         qualitiesRequestFailed: (state, action) => {
@@ -26,14 +28,25 @@ const qualitiesSlice = createSlice({
 const { reducer: qualitiesReducer, actions } = qualitiesSlice;
 const { qualitiesRequested, qualitiesReceved, qualitiesRequestFailed } = actions;
 
-export const loadQualitiesList = () => async (dispatch) => {
-    dispatch(qualitiesRequested());
-    try {
-        const { content } = await qualityService.fetchAll();
-        dispatch(qualitiesReceved(content));
-    } catch (error) {
-        dispatch(qualitiesRequestFailed(error.message));
-    }
+function isOutdated(date) { 
+	if(Date.now() - date > 10 * 60 * 1000) {
+		return true
+	}
+	return false
+}
+
+export const loadQualitiesList = () => async (dispatch, getState) => {
+	const {lastFetch} = getState().qualities
+	if(isOutdated(lastFetch)) {
+		console.log(lastFetch)
+		dispatch(qualitiesRequested());
+		try {
+			const { content } = await qualityService.fetchAll();
+			dispatch(qualitiesReceved(content));
+		} catch (error) {
+			dispatch(qualitiesRequestFailed(error.message));
+		}
+	}
 };
 
 export const getQualities = () => (state) => state.qualities.entities;
