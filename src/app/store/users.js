b8bd36bf09715279195dbc82a110d358c82fbc63
Vitaly <vitalyrubis@gmail.com>
2022-11-07 16:59:26 +0300

    Изменяем ProtectedRoutes

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -28,7 +28,8 @@ const usersSlice = createSlice({
 			state.isLoading = false;
 		},
 		authRequestSuccess: (state, action) => {
-			state.auth = {...action.payload, isLoggedIn: true }
+			state.auth = action.payload
+			state.isLoggedIn = true
 		},
 		authRequestFailed: (state, action) => {
 			state.error = action.payload
@@ -112,6 +113,8 @@ export const getUsersByIds = (userId) => (state) => {
 		return state.users.entities.find(u => u._id === userId)
 	}
 }
+
+export const getIsLoggedIn = () => state => state.users.isLoggedIn
 
 export default usersReducer
 
